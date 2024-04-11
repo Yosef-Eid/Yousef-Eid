63227f4a7985dafdcaddf9e1a8933f9182ec2475
unknown <yosefeid058@gmail.com>
2024-04-11 17:14:11 +0200

    project

--- src/components/side-par/SidePar.js
+++ src/components/side-par/SidePar.js
@@ -12,7 +12,7 @@ import Link from 'next/link';
 
 function SideBar() {
 
-  let styleIcon = "flex flex-col items-center justify-center text-[23px] max-[400px]:text-[22px] cursor-pointe w-10 h-10 max-[400px]:w-8 max-[400px]:h-8 rounded-xl max-[400px]:bg-white bg-gray-100 hover:bg-[#ffb400] hover:text-white cursor-pointer transition-all delay-75 ease-out "
+  let styleIcon = "flex flex-col items-center justify-center text-[23px] max-[400px]:text-[22px] cursor-pointe w-10 h-10 max-[400px]:w-8 max-[400px]:h-8 rounded-xl max-[400px]:bg-white bg-gray-100 min-[400px]:hover:bg-[#ffb400]  min-[400px]:hover:text-white hover:text-[#faa000] cursor-pointer transition-all delay-75 ease-out "
   return (
     <div className='h-svh min-w-16 shadow-2xl flex flex-col items-center justify-center bg-white max-[400px]:fixed bottom-0 max-[400px]:w-full max-[400px]:h-12 z-50 relative '>
       <div className='w-full flex flex-col items-center justify-center gap-10 max-[400px]:gap-[4%]  text-gray-600 max-[400px]:flex-row z-40 max-[400px]:justify-between max-[400px]:px-[5%]' >
